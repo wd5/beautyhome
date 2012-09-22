f79bba210870bc2a53919162067a7ee956d50ca5
Stepa <steeg.xs@gmail.com>
2012-09-22 23:53:14 +1000

    оформление заказа

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
+from django.core.context_processors import csrf
 from django.core.mail.message import EmailMessage
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -170,14 +171,16 @@ class OrderFromView(FormView):
                 reg_form = RegistrationForm(initial={'email': new_order.email, })
             else:
                 reg_form = False
-            # не регистрируем пользователя а просто губы выводим
-            return render_to_response('orders/order_form_final.html',
-                    {'order': new_order, 'request': request, 'user': request.user,
-                     'reg_form': reg_form, 'phonenum': phonenum, })
+
+            c = {'order': new_order, 'request': request, 'user': request.user,
+                                 'reg_form': reg_form, 'phonenum': phonenum, }
+            c.update(csrf(request))
+            return render_to_response('orders/order_form_final.html', c)
         else:
-            return render_to_response(self.template_name,
-                    {'order_form': order_form, 'request': request, 'user': request.user, 'cart_total': cart.get_str_total(),
-                     'selfcarting_text': selfcarting_text, 'phonenum': phonenum })
+            c = {'order_form': order_form, 'request': request, 'user': request.user, 'cart_total': cart.get_str_total(),
+                                 'selfcarting_text': selfcarting_text, 'phonenum': phonenum }
+            c.update(csrf(request))
+            return render_to_response(self.template_name, c)
 
     def get(self, request, *args, **kwargs):
         form_class = self.get_form_class()
